ebad8e8e6ab5b3e9af3b13698b7ccfdd901c25bd
Amr109 <grand.109.master@gmail.com>
2021-09-18 17:33:22 +0300

    first commit

---
+++ components/navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full bg-gray-900">
+    <div class="text-gray-100 w-full flex justify-center">
+      This is My Starter Project
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div><navbar /></div>
+    <div>
+      <Tutorial />
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from '~/components/navbar.vue'
+export default {
+  components: { navbar },
+}
+</script>
